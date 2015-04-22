3fe69ac0361e42488e7a5c38e49fc71f115858bc
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-04-21 18:54:32 -0700

    Add password_digest and picupldr.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421024736) do
+ActiveRecord::Schema.define(version: 20150421205422) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 20150421024736) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
