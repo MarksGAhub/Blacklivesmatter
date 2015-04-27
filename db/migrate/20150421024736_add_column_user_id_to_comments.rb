8343a7260fa4912df888598617944324ccd9eae8
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-04-26 21:16:05 -0700

    fixed foreign keys and dbmigrated to postgresql.

--- db/migrate/20150421024736_add_column_user_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnUserIdToComments < ActiveRecord::Migration
-  def change
-    add_reference :comments, :users, index: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424233921) do
+ActiveRecord::Schema.define(version: 20150427035324) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
     t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "users_id"
     t.integer  "post_id"
+    t.integer  "user_id"
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["users_id"], name: "index_comments_on_users_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -41,4 +44,6 @@ ActiveRecord::Schema.define(version: 20150424233921) do
     t.string   "password_digest"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
